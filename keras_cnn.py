ae78e9b7feae806ccc3c4036bde548a3dff9dd94
Akash S <akash.shingte@gmail.com>
2018-11-28 11:23:22 -0500

    Convolution Neural Nets

--- keras_cnn.py
+++ keras_cnn.py
@@ -37,18 +37,22 @@ y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
 model = Sequential()
-model.add(Conv2D(32, kernel_size=(5, 5), strides=(1, 1),
-                 activation='relu',
+model.add(Conv2D(32, kernel_size=(3,3), strides=(1, 1),
+                 activation='tanh',
                  input_shape=input_shape))
 model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-model.add(Conv2D(64, (5, 5), activation='relu'))
+model.add(Conv2D(64, (3,3), activation='tanh'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Flatten())
-model.add(Dense(1000, activation='relu'))
+model.add(Dense(1000, activation='tanh'))
 model.add(Dense(num_classes, activation='softmax'))
-
+learning_rate = 0.01
+decay_rate = learning_rate / epochs
+momentum = 0.8
 model.compile(loss=keras.losses.categorical_crossentropy,
-              optimizer=keras.optimizers.Adam(),
+              optimizer=keras.optimizers.SGD(lr=learning_rate,
+                  momentum=momentum, decay=decay_rate,
+                  nesterov=True),
               metrics=['accuracy'])
 
 class AccuracyHistory(keras.callbacks.Callback):
--- load_model.py
+++ load_model.py
@@ -1,16 +1,8 @@
-from __future__ import print_function
-import matplotlib.pyplot as plt
-import numpy as np
-import keras
-from keras.datasets import mnist
-from keras.models import Sequential, load_model
-from keras.layers import Flatten, Dense, Dropout
-from keras.layers import MaxPooling2D, ZeroPadding2D
-from keras.layers.convolutional import Convolution2D
-from keras.layers.core import Activation
-from keras import backend as K
-from keras.utils import np_utils
+from keras.models import load_model
 
+batch_size = 128
+num_classes = 10
+epochs = 10
 
 # input image dimensions
 img_x, img_y = 28, 28
@@ -33,36 +25,11 @@ print('x_train shape:', x_train.shape)
 print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
 
-model = load_model('cnn_arch_1')
-print(model)
-
-img_to_visualize = x_train[0]
-img_to_visualize = np.expand_dims(img_to_visualize, axis=0)
-
-def layer_to_visualize(layer):
-    inputs = model.inputs
-
-    _convout1_f = K.function(inputs, [layer.output])
-    def convout1_f(X):
-        # The [0] is to disable the training phase flag
-        return _convout1_f([X])
-
-    convolutions = convout1_f(img_to_visualize)
-    convolutions = np.squeeze(convolutions)
-
-    print ('Shape of conv:', convolutions.shape)
-
-    n = convolutions.shape[2]
-    n = int(np.ceil(np.sqrt(n)))
-
-    # Visualization of each filter of the layer
-    fig = plt.figure(figsize=(12,8))
-    for i in range(len(convolutions[0,0,:])):
-        ax = fig.add_subplot(n,n,i+1)
-        ax.imshow(convolutions[:,:,i], cmap='gray')
-    plt.show()
+# convert class vectors to binary class matrices - this is for use in the
+# categorical_crossentropy loss below
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
 
 
-print(len(model.layers[:-3]))
-for layer in model.layers[:-3]:
-    layer_to_visualize(layer)
+model  = load_model('cnn_arch_1')
+model2 = load_model('cnn_arch_1_one_layer_less')
